3117337e3cf50627134d00f47a037c37d14e5488
Vadim Kozlov <kozlov.vev@gmail.com>
2022-07-05 17:52:07 -0600

    Fix vue config file

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,4 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-  ? '/mexico-surfing/'
-  : '/'
-  
-}
-const { defineConfig } = require('@vue/cli-service')
-
-module.exports = defineConfig({
-
-lintOnSave:false
-
-})
+  publicPath: process.env.NODE_ENV === 'production' ? '/mexico-surfing/' : '/',
+  lintOnSave:false
+};
